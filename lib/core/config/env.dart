178d5360581975d3db78bfd9d44e2d52648315d5
Ivan Cardona <2021ivancardona@gmail.com>
2025-07-27 01:00:41 -0500

    - set default value in ENV for test purposes

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get dbName => dotenv.env['DB_NAME'] ?? 'default.db';
+  static String get dbName => dotenv.env['DB_NAME'] ?? 'your_store_app.db';
   static int get dbVersion => int.tryParse(dotenv.env['DB_VERSION'] ?? '1') ?? 1;
-  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? 'https://dummyjson.com';
 }
