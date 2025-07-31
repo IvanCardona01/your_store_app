9ff713d09cb5b417fb1e1a6c4d9ddfead61467ec
Ivan Cardona <2021ivancardona@gmail.com>
2025-07-31 12:42:22 -0500

    - removed unnussed routes

--- lib/app/router/app_routes.dart
+++ lib/app/router/app_routes.dart
@@ -6,16 +6,10 @@ class AppRoutes {
   
   // Products routes
   static const String products = '/products';
-  static const String productDetail = '/products/:productId';
   
   // Cart and checkout routes
   static const String cart = '/cart';
-  static const String checkout = '/checkout';
-  static const String orderSuccess = '/order-success';
   
   // User profile routes
   static const String profile = '/profile';
-  static const String editProfile = '/profile/edit';
-  static const String orderHistory = '/profile/orders';
-  
 }
